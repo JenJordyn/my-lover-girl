e369798a57fc6e0d8e98d5fc7d814fb523511109
JenJordyn <JenniferBridges02@gmail.com>
2025-02-02 14:23:55 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-/* script.js */
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const messages = [
         "Hey Baby, I just want to say that I’m really proud of you.",
         "I see how hard you’ve been working, and even though I can’t be there to watch you perform, I still want to find ways to show my support.",
@@ -7,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
         "Every time I get a glimpse of what you do, it kind of puts things into perspective for me.",
         "It’s not that I wish I could do what you do (I think I'd shrivel up from fear), but more that I know I never could",
         "—and I think that’s what makes it so impressive to me.",
-        "We're all on put on this earth to fulfill a different purpose. Yours is something that takes guts. You choose passion every single day, and that’s something I love about you.",
+        "We're all put on this earth to fulfil a different purpose. Yours is something that takes guts. You choose passion every single day, and that’s something I love about you.",
         "We both know I overthink everything, but watching you go all in on something you love reminds me why it matters to take risks for the things that mean the most.",
         "For things that we're passionate about, a part of me feels like I lost that a long time ago. But you remind me why passion is so important for purpose.",
         "And I love getting to see you live your purpose."
